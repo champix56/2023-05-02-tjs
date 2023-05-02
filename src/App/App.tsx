df084749b1cd772ee888e4b2cef3c1008367e777
champix56 <desorbaix@free.fr>
2023-05-02 15:57:51 +0200

    before prop types

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React from "react";
 // import logo from './logo.svg';
-import './App.css';
-import Button from './components/ui/Button/Button';
-import Button2 from './Button/Button';
+import "./App.css";
+import Button from "./components/ui/Button/Button";
 
 function App() {
   return (
     <div className="App" data-testid="App">
-      <Button text="Bejamin" onClick={function(){console.log('hello');}}/>
-      <Button2 text="button"/>
+      <Button
+        text={'Hello'}
+        onClick={function () {
+          console.log("hello");
+        }}
+      >
+          <img src="https://cdn1.iconfinder.com/data/icons/bootstrap-vol-3/16/menu-button-wide-256.png" />
+          <br /> 
+          hello
+      </Button>
+
+      <Button text="button" />
     </div>
   );
 }
--- src/App/components/ui/Button/Button.js
+++ src/App/components/ui/Button/Button.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from './Button.module.css';
+
 const Button = (props) => {
   console.log(props);
   return <button className={styles.Button} onClick={(evt)=>{
     console.log(evt);
     props.onClick('le roi albert');
-  }}>{props.text}</button>;
+  }}>{props.children}</button>;
 };
 export default Button;
