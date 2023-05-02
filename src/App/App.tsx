744c75c31298f876c3dbf0548c7002b313bffcd4
champix56 <desorbaix@free.fr>
2023-05-02 15:05:43 +0200

    avec css module

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Button from './components/ui/Button/Button';
+import Button2 from './Button/Button';
 
 function App() {
   return (
     <div className="App" data-testid="App">
-      DEMAT 
       <Button text="Bejamin" onClick={function(){console.log('hello');}}/>
-      <button>Reel</button>
-      <button>Reel</button>
-      <Button text="button"/>
-      <button>Reel</button>
-
+      <Button2 text="button"/>
     </div>
   );
 }
--- src/App/components/ui/Button/Button.js
+++ src/App/components/ui/Button/Button.js
@@ -1,8 +1,8 @@
 import React from "react";
-
+import styles from './Button.module.css';
 const Button = (props) => {
   console.log(props);
-  return <button className="Button" onClick={(evt)=>{
+  return <button className={styles.Button} onClick={(evt)=>{
     console.log(evt);
     props.onClick('le roi albert');
   }}>{props.text}</button>;
