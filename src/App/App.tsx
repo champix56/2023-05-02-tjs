5ccd43b622ac6ca0852ef6dd7a9f20608032c1a5
champix56 <desorbaix@free.fr>
2023-05-04 00:20:44 +0200

    implem modal

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,11 +5,14 @@ import WFirstGrow from "./components/layout/WFirstGrow/WFirstGrow";
 import MemeForm from "./components/MemeForm/MemeForm";
 import Footer from "./components/ui/Footer/Footer";
 import { MemeSVGViewer, emptyMeme, MemeInterface } from "orsys-tjs-meme";
+import { ReduxModal } from "./components/Modal/Modal";
+import store from "./store/store";
+import { showModal } from "./store/modal";
 
 interface iAppState {
   meme: MemeInterface;
 }
-interface IAppProps {}
+interface IAppProps { }
 
 export default class App extends React.Component<IAppProps, iAppState> {
   constructor(props: IAppProps) {
@@ -19,21 +22,29 @@ export default class App extends React.Component<IAppProps, iAppState> {
 
   render() {
     return (
-      <div className="App" data-testid="App">
-        <Header />
-        <Navbar />
-        {JSON.stringify(this.state)}
-        <WFirstGrow>
-          <MemeSVGViewer meme={this.state.meme} image={undefined} />
-          <MemeForm
-            meme={this.state.meme}
-            onMemeChange={(meme: MemeInterface) => {
-              this.setState({ meme: meme });
-            }}
-          />
-        </WFirstGrow>
-        <Footer />
-      </div>
+      <>
+        <button onClick={function () { store.dispatch(showModal('titleMess', 'COUCOCU')) }}>message</button>
+        <button onClick={function () {
+
+          store.dispatch(showModal('titl', 'COUCOCU', () => { console.log('ok') }, () => { }))
+
+        }}>dialog</button>
+        <div className="App" data-testid="App">
+          <Header />
+          <Navbar />
+          {JSON.stringify(this.state)}
+          <WFirstGrow>
+            <MemeSVGViewer meme={this.state.meme} image={undefined} />
+            <MemeForm
+              meme={this.state.meme}
+              onMemeChange={(meme: MemeInterface) => {
+                this.setState({ meme: meme });
+              }}
+            />
+          </WFirstGrow>
+          <Footer />
+        </div>
+        <ReduxModal /></>
     );
   }
 }
