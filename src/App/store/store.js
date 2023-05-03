c444d77df357baff7d1231d409f140293331fc56
champix56 <desorbaix@free.fr>
2023-05-04 01:40:23 +0200

    configured store

--- src/App/store/store.js
+++ src/App/store/store.js
@@ -1,6 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createAsyncThunk } from "@reduxjs/toolkit";
 import { createStore, combineReducers } from "@reduxjs/toolkit";
 import modalReducer from "./modal";
+import memeReducer from "./meme.slice";
+import listsReducer from './list.slice'
+createAsyncThunk()
 
 
-export default createStore(combineReducers({modal:modalReducer}),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+export default configureStore({
+  reducer: combineReducers({ meme: memeReducer, modal: modalReducer , lists:listsReducer}),
+});
+/*
+export default createStore(
+  combineReducers({ modal: modalReducer }),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);*/
