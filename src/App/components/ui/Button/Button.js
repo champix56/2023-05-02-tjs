62c2d7a5626f2c3a1119295f41005a3e380c48eb
champix56 <desorbaix@free.fr>
2023-05-03 09:08:14 +0200

    etat du button pour clicked

--- src/App/components/ui/Button/Button.js
+++ src/App/components/ui/Button/Button.js
@@ -1,23 +1,33 @@
-import React from "react";
-import PropTypes from 'prop-types';
-import styles from './Button.module.css';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import styles from "./Button.module.css";
 
 const Button = (props) => {
+  const [isClicked, setIsClicked] = useState(false);
   console.log(props);
-  return <button style={{...props.style,backgroundColor:props.backgroundColor}} className={styles.Button} onClick={(evt)=>{
-    console.log(evt);
-    props.onClick('le roi albert');
-  }}>{props.children}</button>;
+  return (
+    <button
+      style={{ ...props.style, backgroundColor: props.backgroundColor }}
+      className={isClicked?styles.Button+' '+styles.clicked:styles.Button}
+      onClick={(evt) => {
+        setIsClicked(true);
+        console.log(evt);
+        props.onClick("le roi albert");
+      }}
+    >
+      {props.children}
+    </button>
+  );
 };
-Button.propTypes={
+Button.propTypes = {
   onClick: PropTypes.func.isRequired,
   children: PropTypes.any.isRequired,
   backgroundColor: PropTypes.string,
   style: PropTypes.object,
 };
-Button.defaultProps={
-  children:'Hello',
-  onClick: ()=>{},
-  backgroundColor: 'skyblue'
-}
+Button.defaultProps = {
+  children: "Hello",
+  onClick: () => {},
+  backgroundColor: "skyblue",
+};
 export default Button;
