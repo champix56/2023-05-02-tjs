ceae5ca2d61a2fa047b2f0807f610cbd54e2e656
champix56 <desorbaix@free.fr>
2023-05-03 21:25:28 +0200

    management du state pour deploiement enfantS

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,26 +2,35 @@ import React, { Component } from "react";
 import Header from "./components/ui/Header/Header";
 import Navbar from "./components/ui/Navbar/Navbar";
 import WFirstGrow from "./components/layout/WFirstGrow/WFirstGrow";
-import MemeForm from "./components/MemeForm/MemeForm"
+import MemeForm from "./components/MemeForm/MemeForm";
 import Footer from "./components/ui/Footer/Footer";
-import { MemeSVGViewer,emptyMeme } from "orsys-tjs-meme";
+import { MemeSVGViewer, emptyMeme, MemeInterface } from "orsys-tjs-meme";
 
+interface iAppState {
+  meme: MemeInterface;
+}
+interface IAppProps {}
 
-type Props = {};
-
-type State = {};
-
-export default class App extends Component<Props, State> {
-  state = {meme:emptyMeme};
+export default class App extends React.Component<IAppProps, iAppState> {
+  constructor(props: IAppProps) {
+    super(props);
+    this.state = { meme: emptyMeme };
+  }
 
   render() {
     return (
       <div className="App" data-testid="App">
         <Header />
         <Navbar />
+        {JSON.stringify(this.state)}
         <WFirstGrow>
           <MemeSVGViewer meme={this.state.meme} image={undefined} />
-          <MemeForm />
+          <MemeForm
+            meme={this.state.meme}
+            onMemeChange={(meme: MemeInterface) => {
+              this.setState({ meme: meme });
+            }}
+          />
         </WFirstGrow>
         <Footer />
       </div>
