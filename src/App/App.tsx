81d9b3d562552c579aa4c9da6be73d5c8bce8071
champix56 <desorbaix@free.fr>
2023-05-04 11:33:28 +0200

    fetch in component

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,17 +4,27 @@ import Navbar from "./components/ui/Navbar/Navbar";
 import WFirstGrow from "./components/layout/WFirstGrow/WFirstGrow";
 import MemeForm from "./components/MemeForm/MemeForm";
 import Footer from "./components/ui/Footer/Footer";
-import { MemeSVGViewer, emptyMeme, MemeInterface } from "orsys-tjs-meme";
+import { MemeSVGViewer, emptyMeme, MemeInterface, ImageInterface } from "orsys-tjs-meme";
 
 interface iAppState {
   meme: MemeInterface;
+  images:Array<ImageInterface>;
 }
 interface IAppProps {}
 
 export default class App extends React.Component<IAppProps, iAppState> {
   constructor(props: IAppProps) {
     super(props);
-    this.state = { meme: emptyMeme };
+    this.state = { meme: emptyMeme, images:[] };
+  }
+  componentDidMount(): void {
+    fetch('http://localhost:5679/images',{
+      headers:{
+        Origin: 'https://localhost:5679'
+      }
+    })
+      .then(r=>r.json())
+      .then(arr=>this.setState({images:arr}));
   }
 
   render() {
