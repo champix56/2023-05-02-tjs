c61400507f6a80d26e219a17144e224279de3648
champix56 <desorbaix@free.fr>
2023-05-04 09:04:17 +0200

    use Conneted component

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,24 +2,25 @@ import React, { Component } from "react";
 import Header from "./components/ui/Header/Header";
 import Navbar from "./components/ui/Navbar/Navbar";
 import WFirstGrow from "./components/layout/WFirstGrow/WFirstGrow";
-import MemeForm from "./components/MemeForm/MemeForm";
+import MemeForm, { StoreMemeForm } from "./components/MemeForm/MemeForm";
 import Footer from "./components/ui/Footer/Footer";
 import { MemeSVGViewer, emptyMeme, MemeInterface } from "orsys-tjs-meme";
 import { ReduxModal } from "./components/Modal/Modal";
 import store from "./store/store";
 import { showModal } from "./store/modal";
+import StoreMemeSVGViewer from "./components/ui/MemeSVGViewer/MemeSVGViewer";
+import { fetchImages } from "./store/list.slice";
 
 interface iAppState {
-  meme: MemeInterface;
+
 }
 interface IAppProps { }
 
 export default class App extends React.Component<IAppProps, iAppState> {
-  constructor(props: IAppProps) {
-    super(props);
-    this.state = { meme: emptyMeme };
-  }
 
+componentDidMount(): void {
+ store.dispatch(fetchImages());
+}
   render() {
     return (
       <>
@@ -32,15 +33,9 @@ export default class App extends React.Component<IAppProps, iAppState> {
         <div className="App" data-testid="App">
           <Header />
           <Navbar />
-          {JSON.stringify(this.state)}
           <WFirstGrow>
-            <MemeSVGViewer meme={this.state.meme} image={undefined} />
-            <MemeForm
-              meme={this.state.meme}
-              onMemeChange={(meme: MemeInterface) => {
-                this.setState({ meme: meme });
-              }}
-            />
+            <StoreMemeSVGViewer />
+            <StoreMemeForm />
           </WFirstGrow>
           <Footer />
         </div>
