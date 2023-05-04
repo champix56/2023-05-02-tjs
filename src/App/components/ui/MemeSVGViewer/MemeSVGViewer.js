db542eb6d7b275f94c8711ab18375073cff225d6
champix56 <desorbaix@free.fr>
2023-05-04 09:03:53 +0200

    format

--- src/App/components/ui/MemeSVGViewer/MemeSVGViewer.js
+++ src/App/components/ui/MemeSVGViewer/MemeSVGViewer.js
@@ -1,11 +1,13 @@
-import { MemeSVGViewer } from 'orsys-tjs-meme';
-import React from 'react';
-import { useSelector } from 'react-redux';
+import { MemeSVGViewer } from "orsys-tjs-meme";
+import React from "react";
+import { useSelector } from "react-redux";
 
 export default function StoreMemeSVGViewer() {
-    const meme=useSelector(s=>s.meme)
-    const image=useSelector(s=>s.lists.images.find(i=>i.id===s.meme.imageId))
-  return (
-    <MemeSVGViewer meme={meme} image={image}/>
-  );
+  const meme = useSelector((s) => s.meme);
+  const image = useSelector((s) => {
+    return s.lists.images.find((i) => {
+      return i.id === meme.imageId;
+    });
+  });
+  return <MemeSVGViewer meme={meme} image={image} basePath="" />;
 }
