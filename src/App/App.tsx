60f557847174991d9ea2edab219ffa70ea9d8ed4
champix56 <desorbaix@free.fr>
2023-05-09 18:49:14 +0200

    delete unused import

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import Header from "./components/ui/Header/Header";
 import Navbar from "./components/ui/Navbar/Navbar";
 import WFirstGrow from "./components/layout/WFirstGrow/WFirstGrow";
-import MemeForm from "./components/MemeForm/MemeForm";
 import Footer from "./components/ui/Footer/Footer";
-import { MemeSVGViewer, emptyMeme, MemeInterface, ImageInterface } from "orsys-tjs-meme";
-import store from "./store/store";
-import { addImages } from "./store/ressources";
 import StoredMemeForm from "./components/MemeForm/StoredMemeForm";
 import StoredMemeSVGViewer from "./components/ui/storedMemeSVGViewer/storedMemeSVGViewer";
 
